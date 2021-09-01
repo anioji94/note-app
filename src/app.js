48cb77e2038d929c7905875a5227ec7933722ba7
anioji94 <ojhaa94@gmail.com>
2021-09-01 21:05:28 +0100

    notes can be added to the bottom of the page

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const submit = document.querySelector('#submit-note')
     const title = document.querySelector('#note-title')
     const body = document.querySelector('#note-body')
+    const noteList = document.getElementById("note-list");
 
     // addNote
     submit.addEventListener('click', () => {
         event.preventDefault()
-        // Read innerText of title and body
+        // Read value of title and body
         note.addNote(title.value, body.value)
-        console.log(title.value + body.value)
-        console.log(note)
+        noteList.innerHTML += `<div id="note">\n<h3>${title.value}</h3>\n<p>${body.value}</p>\n</div>`
+        title.value = ""
+        body.value = ""
+
     })
+
 })
 
 // note-title
